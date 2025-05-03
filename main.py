a1733656857b7a37ec3b78e67c6e8e2c7e584800
Mangat Singh Toor | ਮੰਗਤ ਸਿੰਘ ਤੂਰ <immangat@gmail.com>
2025-05-02 23:16:29 -0700

    add help command

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import os
 import discord
 from dotenv import load_dotenv
@@ -10,17 +9,31 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 intents = discord.Intents.all()
 client = discord.Client(command_prefix='!', intents=intents)
 
+# Help message as a separate variable
+HELP_MESSAGE = (
+    "Hello! I'm your friendly bot. Here are some commands you can use:\n"
+    "`!help` - Displays this message with usage information.\n"
+    "`New Homework Item` - Sends a fun reminder about homework.\n"
+    "Enjoy using the bot!"
+)
+
+
 @client.event
 async def on_ready():
-  print('We have logged in as {0.user}'.format(client))
+    print('We have logged in as {0.user}'.format(client))
+
 
 # this is the code we will use first to test the connection
 @client.event
 async def on_message(message):
-  if message.author == client.user:
-    return
-  if message.content.startswith("New Homework Item"):
-    await message.channel.send("Say goodbye to your weekend!")
+    if message.author == client.user:
+        return
+    if message.content.startswith('!help'):
+        await message.channel.send(HELP_MESSAGE)
+
+    if message.content.startswith("New Homework Item"):
+        await message.channel.send("Say goodbye to your weekend!")
+
 
 # start the bot
 client.run(TOKEN)
