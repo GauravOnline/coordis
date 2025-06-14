d76ad5a6907ae0cc1cdeaaf33ae05cf8d9aadf99
gawg02 <g_dog02@hotmail.com>
2025-06-13 21:27:55 -0700

    Update constants.py

--- core/constants.py
+++ core/constants.py
@@ -4,3 +4,4 @@ FEEDBACK_MESSAGE_DISPLAY_TIME = 5 # e.g. Feedback messages, success confirmation
 ERROR_MESSAGE_DISPLAY_TIME = 20 # e.g Error message, incorrect usage
 USAGE_MESSAGE_DISPLAY_TIME = 10  # e.g. Usage instructions, help messages
 ALARM_MESSAGE_DISPLAY_TIME = 15 # Alarm message duration
+ALARM_MESSAGE_DISPLAY_TIME = 15 # Alarm message duration
